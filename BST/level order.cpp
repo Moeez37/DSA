97a9f9f1fda2ef28341ee798552a230a6554ebef
Moeez Hayder37 <72216020+Moeez37@users.noreply.github.com>
2023-03-31 02:46:17 +0500

    BST
    Signed-off-by: Moeez Hayder37 <72216020+Moeez37@users.noreply.github.com>

---
+++ BST/level order.cpp
@@ -0,0 +1,66 @@
+/* Iterative program to print levels line by line */
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// A Binary Tree Node
+struct node {
+	struct node* left;
+	int data;
+	struct node* right;
+};
+
+// Iterative method to do level order traversal
+// line by line
+void printLevelOrder(node* root)
+{
+	// Base Case
+	if (root == NULL)
+		return;
+	
+	
+	// Create an empty queue for level order traversal
+	queue<node*> q;
+	q.push(root);
+	while(q.empty()==false)
+	{
+		
+		
+		int count=q.size();
+		while(count>0)
+		{		node* temp=q.front();q.pop();
+			cout<<temp->data<<" ";
+			
+			if(temp->left!=NULL)q.push(temp->left);
+			if(temp->right!=NULL)q.push(temp->right);
+			count--;
+		}
+		cout<<endl;
+		
+	}
+}
+
+// Utility function to create a new tree node
+node* newNode(int data)
+{
+	node* temp = new node;
+	temp->data = data;
+	temp->left = NULL;
+	temp->right = NULL;
+	return temp;
+}
+
+// Driver program to test above functions
+int main()
+{
+	// Let us create binary tree shown above
+	node* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+
+	printLevelOrder(root);
+	return 0;
+}
+
