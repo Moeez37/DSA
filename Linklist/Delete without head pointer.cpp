3e32b73626bb0b1638c00805e88170df3100e13a
Moeez Hayder37 <72216020+Moeez37@users.noreply.github.com>
2023-04-03 16:17:46 +0500

    problem of deletion
    Signed-off-by: Moeez Hayder37 <72216020+Moeez37@users.noreply.github.com>

---
+++ Linklist/Delete without head pointer.cpp
@@ -0,0 +1,83 @@
+
+#include<iostream>
+using namespace std;
+
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}*head;
+
+Node *findNode(Node* head, int search_for)
+{
+    Node* current = head;
+    while (current != NULL)
+    {
+        if (current->data == search_for)
+            break;
+        current = current->next;
+    }
+    return current;
+}
+
+
+void insert()
+{
+    int n,i,value;
+    Node *temp;
+    scanf("%d",&n);
+
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&value);
+        if(i==0)
+        {
+            head=new Node(value);
+            temp=head;
+            continue;
+        }
+        else
+        {
+            temp->next= new Node(value);
+            temp=temp->next;
+            temp->next=NULL;
+        }
+    }
+}
+
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    cout << endl;
+}
+
+
+
+
+class Solution
+{
+    public:
+    void deleteNode(Node *del)
+    {
+       del->data=del->next->data;
+       Node *temp;
+       temp=del->next;
+       del->next=del->next->next;
+       delete(temp);
+    }
+
+};
+
+int main()
+{
+	return 0;
+}
